2d72d9d9f2adf09b87e24969771bfbbe6ddcce8d
cop <32881179+cop-discord@users.noreply.github.com>
2024-06-30 10:50:02 -0700

    yes

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+
+from python_dotenv import load_dotenv
+load_dotenv(verbose = True)
+import os
+
 PREFIX = ","
-TOKEN = ""
+TOKEN = os.environ["TOKEN"]
 CHANNEL = None
 uri = "postgresql://postgres.vothbutfvvjkjxlcvwwo:fuckyou123$$!$!$$!$!$!@@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
