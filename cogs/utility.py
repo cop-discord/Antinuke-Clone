e9596223271666a23847cbb8d2b26d1196575ea2
keron <141990328+aiosqlite@users.noreply.github.com>
2024-06-30 12:38:07 -0400

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,1 +1,14 @@
+from discord import User, Member, Embed
+from discord.ext.commands import command, group, Context, Cog
+from backend import BOT, SELF, USER
 
+class utility(Cog):
+    def __init__(self: SELF, bot: BOT):
+        self.bot = bot
+
+    @command(name = "afk", aliases=["away", "zzz"], description = "finally get away from discord")
+    async def afk(self: SELF, ctx: Context, *, user: USER):
+
+
+async def setup(bot: BOT):
+    await bot.add_cog(utility(bot))
