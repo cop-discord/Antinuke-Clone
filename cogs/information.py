ea8df79877a3d7eae335cf075d0f542a019d6a62
keron <141990328+aiosqlite@users.noreply.github.com>
2024-06-30 12:29:27 -0400

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -33,7 +33,28 @@ class Information(Cog):
         view.add_item(Button(label = "WEBP", url = user.banner.with_format("WEBP").url))
         view.add_item(Button(label = "JPG", url = user.banner.with_format("JPG").url))
         return await ctx.send(embed = embed, view = view)
-    
+
+    @command(name = "userinfo", description = "fetch basic information on a given userid or username")
+    async def userinfo(self, ctx: Context, *, user: User = None):
+        if user is None:
+            if ctx.message.mentions:
+                user = ctx.message.mentions[0]
+            else:
+                user = await self.bot.fetch_user(ctx.message.content.split()[-1])
+        mutual_guilds = [guild for guild in self.bot.guilds if guild.get_member(user.id)]
+        embed = Embed(color=self.bot.color)
+        embed.set_thumbnail(url=user.avatar.url)
+        embed.add_field(name="Joined Discord", value=f"<t:{int(user.created_at.timestamp())}:f>")
+        if isinstance(user, Member):
+            embed.title = f"{user.display_name} @{user.name}"
+            embed.add_field(name="joined server", value=f"<t:{int(user.joined_at.timestamp())}:f>")
+        else:
+            embed.title = f"{user.name}"
+        embed.set_footer(text=f"mutual guilds: {len(mutual_guilds)}")
+        embed.set_author(name=str(user.id), icon_url=user.avatar.url)
+        await ctx.send(embed=embed)
+
+
 async def setup(bot: BOT):
     await bot.add_cog(Information(bot))
 
