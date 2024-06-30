5c42e19f22de4aade368cd91b0a9279842d32417
keron <141990328+aiosqlite@users.noreply.github.com>
2024-06-30 12:58:33 -0400

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,8 +1,9 @@
-from discord import User, Member, Embed
+from discord import User, Member, Embed, PermissionOverwrite
 from discord.ext.commands import command, Context, Cog
 from backend import BOT, SELF, USER
 from backend.listeners import listeners
-class utility(Cog):
+
+class Utility(Cog):
     def __init__(self: SELF, bot: BOT):
         self.bot = bot
 
@@ -12,7 +13,18 @@ class utility(Cog):
         await self.bot.db.execute("INSERT INTO afk (user_id, reason) VALUES ($1, $2)", ctx.author.id, reason)
         await ctx.success(f"{ctx.author.mention} you're now afk with reason: **{reason}**")
 
-async def setup(bot: BOT):
-    await bot.add_cog(utility(bot))
-    awaut bot.add_cog(listeners(bot))
+    @command(name="rmute", aliases=["rm"], description="remove permissions to add reaction in all channels")
+    async def rmute(self: SELF, ctx: Context, user: USER):
+        for channel in ctx.guild.channels:
+            await channel.set_permissions(user, add_reactions=False)
+        await ctx.success(f"{user.mention} has been revoked from adding reactions anywhere")
 
+    @command(name="rmute", aliases=["ru"], description="unreaction mute a discordian")
+    async def runmute(self: SELF, ctx: Context, user: USER):
+        for channel in ctx.guild.channels:
+            await channel.set_permissions(user, overwrite=None)
+        await ctx.success(f"{user.mention} has allowed to react again")
+
+async def setup(bot: BOT):
+    await bot.add_cog(Utility(bot))
+    await bot.add_cog(listeners(bot))
